bb7a81b97acef3ec92a78657e42a7d8270d1dd9e
Andre1312 <alcbarros@gmail.com>
2022-05-19 18:23:42 -0300

    adionando modulos

---
+++ Controle_Estoque/modulo_peca.py
@@ -0,0 +1,34 @@
+"""
+    Modulo peça - funções para CRUD no arquivo peca.csv
+    
+    campos:
+    id_peca
+    nome
+    tipo
+    data_atualizacao
+    ativo
+      
+    
+"""
+import csv
+import time
+import datetime
+import prettytable as PT
+
+def adicionarPeca():
+    pass
+
+def editarPeca():
+    pass
+
+def listarPeca():
+    pass
+
+def apagarPeca():
+    pass
+
+def salvarPecaArquivo():
+    pass
+
+def lerPecaArquivo():
+    pass
