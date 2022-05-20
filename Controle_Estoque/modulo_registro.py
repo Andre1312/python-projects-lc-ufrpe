adb1a56cf65a3a2e00baf5beb14f82c452dd1131
Andre1312 <alcbarros@gmail.com>
2022-05-20 10:17:00 -0300

    last update

--- Controle_Estoque/modulo_registro.py
+++ Controle_Estoque/modulo_registro.py
@@ -16,6 +16,7 @@ PROJETO 2VA - Controle de Estoque
     qtde_adicionada
     qtde_removida
     data_atualizacao
+    ativo
     
 '''      
     
@@ -26,19 +27,81 @@ import datetime
 import prettytable as PT
 
 def adicionarEstoque():
-    pass
+    registro = []
+    registros = lerRegistro()
+    print('ADICIONAR REGISTRO')
+    print('¬¬¬¬¬¬¬¬¬¬¬¬¬¬')
+    id_registro = str(input('Entre com o ID de registro (S sai...): ')).lower()
+    if id_registro in ['s']:
+        return
+        # testa se id_registro é unico
+    for linha in registros:
+        if id_registro == linha[0]:
+            print('Peça já existe ! Digite um novo ID...')
+            time.sleep(1)
+            MUT.limpaTela()
+            adicionarRegistro()
+    
+    id_usuario = str(input('Entre com o ID Usuario da registro: ')).title()
+    id_peca = str(input('Entre com o ID Peça da registro: ')).upper()
+    qtde_inicial = str(int(input('Entre com a quantidade inicial: ')))
+    qtde_adicionada = str(int(input('Entre com a quantidade adicionada: ')))
+    qtde_removida = str(int(input('Entre com a quantidade removida: ')))
+    data_atualizacao = datetime.datetime.now()
+    data_atualizacao = data_atualizacao.strftime("%Y/%m/%d %H:%M:%S")
+    ativo = 's'
+    
+    registro.append(id_registro)
+    registro.append(id_usuario)
+    registro.append(id_peca)
+    registro.append(qtde_inicial)
+    registro.append(qtde_adicionada)
+    registro.append(qtde_removida)
+    registro.append(data_atualizacao)
+    registro.append(ativo)
+    
+    salvarRegistro(registro)
 
 def editarEstoque():
     pass
 
 def listarEstoque():
-    pass
+    print('LISTA ESTOQUE')
+    print('¬¬¬¬¬¬¬¬¬¬¬¬¬')
+    print()
+    registros=lerEstoque()
+    tabela = PrettyTable()
+    tabela.field_names=["ID","ID_USUARIO","ID_PEÇA","QTDE_INICIAL","QTDE ADICIONADA","QTDE REMOVIDA","DATA","ATIVO"]
+        
+    for r in registros:
+        tabela.add_row(r)
+        
+    print(tabela)
+    print(f'Registros: {len(registros)}')
+    print()
+    while True:
+        opcao = input('V para voltar... ').lower()
+        if opcao in ['v']:
+            break
 
 def apagarEstoque():
     pass
 
-def salvarEstoque():
-    pass
+def salvarEstoque(registro):
+    arquivo = 'Controle_Estoque/registro.csv'
+    with open(arquivo,'a',newline='') as arq:
+        arq_csv=csv.writer(arq,delimiter=",")
+        arq_csv.writerow(registro)
 
 def lerEstoque():
-    pass
+    arquivo = 'Controle_Estoque/registro.csv'
+    with open(arquivo,'r',newline='') as arq:
+        registros = csv.reader(arq, delimiter=",")
+        registros = list(registros)
+    return registros
+
+def salvarEstoqueArquivo(registro):
+    arquivo = 'Controle_Estoque/registro.csv'
+    with open(arquivo,'w',newline='') as arq:
+        arq_csv = csv.writer(arq,delimiter=",")
+        arq_csv.writerows(registro)
