389f91a0ffa8660dbb5567e96ed15926377e6a45
Andre1312 <alcbarros@gmail.com>
2022-05-22 21:32:23 -0300

    first commit main app quiz_math

---
+++ Quiz_Math/quiz_math.py
@@ -0,0 +1,26 @@
+"""
+UFRPE - Universidade Federal Rural de Pernambuco
+DC - Departamento de Computação
+Curso: Licenciatura Plena em Computação
+Disciplina: Programação 1
+Professor: Dr. João Paulo Lima
+Aluno: André Luiz Coelho Barros
+
+Projeto: Quiz Math
+Descrição:
+    Jogo de Perguntas e Respostas de Matemática Fundamental.
+    Com três níveis de dificuldade: Fácil, Médio e Dificil.
+
+    Nível Fácil: Operações de Adição e Subtração
+    Nível Médio: Operações de Adição, Subtração, Multiplicação
+    Nível Dificil: Operações de Adição, Subtração, Multiplicação e Divisão
+
+    Cada pergunta possui três respostas, e somente uma está correta.
+
+    Cada resposta correta, adiciona 1 ponto no nível fácil, 5 pontos no nível médio 
+    e 10 pontos no nível dificil.
+
+    
+        
+
+"""
