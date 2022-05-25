5d364faedf721a2bfe920769f77aa5c0124b278d
Andre1312 <alcbarros@gmail.com>
2022-05-24 23:53:46 -0300

    update list filter

--- Quiz/modulo_util.py
+++ Quiz/modulo_util.py
@@ -19,66 +19,32 @@ def limpar_tela():
     os.system('cls' if os.name=='nt' else 'clear')
 
 def preparar_jogo(dificuldade):
+    
+    lista=[]
+    
     if dificuldade == 'facil':
-        print('Jogo com dificuldade FACIL')
-        
-        # chama função carrega perguntas e respostas nivel fácil 
-        # retorna lista de perguntas e respostas nivel facil do arquivo quizmath.csv
-        lista_perguntas_respostas = MA.ler_arquivo_quizmath()
-        for linha in lista_perguntas_respostas:
-            #retira da lista perguntas nivel medio e dificil
-            if linha[1] == 'medio' or linha[1] == 'dificil':
-                idx = lista_perguntas_respostas.index(linha[1])
-                lista_perguntas_respostas.pop(idx)
-            print(linha)
-               
-        time.sleep(1.0)
-        limpar_tela()
-
-        # lista_perguntas_respostas = ler_perguntas_respostas(dificuldade)
-        # lista_perguntas_respostas = []
-        return lista_perguntas_respostas
-
+        print('Jogo com dificuldade nivel FACIL')
     elif dificuldade == 'medio':
-        print('Jogo com dificuldade MEDIO')
-        
-        # chama função carrega perguntas e respostas nivel medio 
-        # retorna lista de perguntas e respostas nivel facil do arquivo quizmath.csv
-        lista_perguntas_respostas = MA.ler_arquivo_quizmath()
-        for linha in lista_perguntas_respostas:
-            #retira da lista perguntas nivel facil e dificil
-            if 'facil' in linha:
-                idx = lista_perguntas_respostas.index('facil')
-                print(f'idx-{idx}')
-                lista_perguntas_respostas.pop(idx)
-            print(linha)
-               
-        b = input('break:>')
-        limpar_tela()
-
-        # lista_perguntas_respostas = ler_perguntas_respostas(dificuldade)
-        # lista_perguntas_respostas = []
-        return lista_perguntas_respostas
-
+        print('Jogo com dificuldade nivel MEDIO')
     elif dificuldade == 'dificil':
-        print('Jogo com dificuldade DIFICIL')
+        print('Jogo com dificuldade nivel DIFICIL')   
         
-        # chama função carrega perguntas e respostas nivel dificil 
-        # retorna lista de perguntas e respostas nivel facil do arquivo quizmath.csv
-        lista_perguntas_respostas = MA.ler_arquivo_quizmath()
-        for linha in lista_perguntas_respostas:
-            #retira da lista perguntas nivel medio e facil
-            if linha[1] == 'medio' or linha[6] == 'facil':
-                idx = lista_perguntas_respostas.index(linha[1])
-                lista_perguntas_respostas.pop(idx)
-            print(linha)
-               
-        time.sleep(1.0)
-        limpar_tela()
-
-        # lista_perguntas_respostas = ler_perguntas_respostas(dificuldade)
-        # lista_perguntas_respostas = []
-        return lista_perguntas_respostas
+    # chama função carrega perguntas e respostas nivel fácil 
+    # retorna lista de perguntas e respostas nivel facil do arquivo quizmath.csv
+    lista_perguntas_respostas = MA.ler_arquivo_quizmath()
+    for linha in lista_perguntas_respostas:
+        #retira da lista perguntas nivel facil, medio e dificil de acordo com a variavel dificuldade
+        if linha[0] == 'id':
+            continue
+        if linha[1] == dificuldade:
+            print(f'{linha}')
+            lista.append(linha)
+    
+    brk = input('break:>')
+    time.sleep(1.0)
+    limpar_tela()
+    
+    return lista
 
 def mostrar_top10():
     print('TOP 10 - Jogadores')
